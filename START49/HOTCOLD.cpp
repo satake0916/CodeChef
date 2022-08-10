b31fb2f11c918e443db4ce9f409d047e2d44a0d2
satake0916 <38389682+satake0916@users.noreply.github.com>
2022-08-10 23:18:58 +0900

    Create HOTCOLD.cpp

---
+++ START49/HOTCOLD.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int c;
+        cin >> c;
+        cout << (c > 20 ? "HOT" : "COLD") << endl;
+    }
+}
