c1863fdc132350c8bde98589c634aebdc9cde916
satake0916 <38389682+satake0916@users.noreply.github.com>
2022-08-11 02:37:28 +0900

    Create POPULATION.cpp

---
+++ START51/POPULATION.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+
+void solve(){
+    int x,y,z;
+    cin >> x >> y >> z;
+    cout << x-y+z << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int T; cin >> T;
+    while(T--) solve();
+    return 0;
+}
